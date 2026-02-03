0d7ccefa4fd1bf37d82d0d23d6a38504733ac993
anjiabdallah <anjiabdallah@gmail.com>
2026-02-03 19:03:41 +0200

    Update migration: add admin password, volunteer date of birth, remove password uniqueness

--- server/src/db/migrations/01-add-date-of-birth-and-admin-password.ts
+++ server/src/db/migrations/01-add-date-of-birth-and-admin-password.ts
@@ -2,17 +2,37 @@ import { Kysely } from 'kysely';
 
 export async function up(db: Kysely<any>): Promise<void> {
   await db.schema
-    .alterTable('volunteer_account')
-    .addColumn('date_of_birth', 'date')
+    .alterTable('admin_account')
+    .addColumn('password', 'varchar(256)', col => col.notNull())
     .execute();
 
   await db.schema
-    .alterTable('admin_account')
-    .addColumn('password', 'varchar(256)')
+    .alterTable('volunteer_account')
+    .addColumn('date_of_birth', 'date', col => col.notNull())
+    .execute();
+
+  await db.schema
+    .alterTable('volunteer_account')
+    .dropConstraint('volunteer_account_password_key')
+    .execute();
+
+  await db.schema
+    .alterTable('organization_account')
+    .dropConstraint('organization_account_password_key')
     .execute();
 }
 
 export async function down(db: Kysely<any>): Promise<void> {
+  await db.schema
+    .alterTable('organization_account')
+    .addUniqueConstraint('organization_account_password_key', ['password'])
+    .execute();
+
+  await db.schema
+    .alterTable('volunteer_account')
+    .addUniqueConstraint('volunteer_account_password_key', ['password'])
+    .execute();
+
   await db.schema
     .alterTable('volunteer_account')
     .dropColumn('date_of_birth')
