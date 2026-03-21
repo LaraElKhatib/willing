6e6bc8905412f57285ed5fe54045d573eb10da1b
anjiabdallah <anjiabdallah@gmail.com>
2026-03-22 00:17:56 +0200

    Revert "fix posting issue"
    This reverts commit e13a726d76086ea8717c306ab950e3c8500869be.

--- client/src/pages/volunteer/VolunteerHome.tsx
+++ client/src/pages/volunteer/VolunteerHome.tsx
@@ -93,6 +93,8 @@ function VolunteerHome() {
   const postingsByCrisisId = new Map<number, PostingWithContext[]>();
 
   allAvailablePostings.forEach((posting) => {
+    if (enrollmentPostingIds.has(posting.id)) return;
+
     const crisisId = getPostingCrisisId(posting);
     if (crisisId === undefined) return;
 
