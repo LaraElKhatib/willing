61c87667e8b334a21a77f065f6c2fba3438b8f6c
MarcHamamji <hamamjimarc@gmail.com>
2026-03-29 21:15:23 +0300

    Fix pgvector extension enabling bug

--- server/src/tests/globalSetup.ts
+++ server/src/tests/globalSetup.ts
@@ -1,9 +1,4 @@
-import { sql } from 'kysely';
-
-export async function setup() {
-  const { default: database } = await import('../db/index.ts');
-  await sql`CREATE EXTENSION IF NOT EXISTS vector;`.execute(database);
-}
+export async function setup() {}
 
 export async function teardown() {
   const { default: database } = await import('../db/index.ts');
