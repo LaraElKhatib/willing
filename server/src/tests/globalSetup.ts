32c80a5182d42f5b6d0b3a186380b3b13b15bf95
MarcHamamji <hamamjimarc@gmail.com>
2026-03-30 00:53:22 +0300

    Delete test uploads folder in teardown

--- server/src/tests/globalSetup.ts
+++ server/src/tests/globalSetup.ts
@@ -1,3 +1,6 @@
+import fs from 'fs';
+
+import config from '../config.ts';
 import { truncateAllTables } from './helpers/database.ts';
 
 export async function setup() {
@@ -7,4 +10,6 @@ export async function setup() {
 export async function teardown() {
   const { default: database } = await import('../db/index.ts');
   await database.destroy();
+
+  await fs.promises.rm(config.UPLOAD_DIR, { recursive: true, force: true });
 }
