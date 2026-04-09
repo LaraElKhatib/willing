49453018f9dc5510ad1ee81848da99e6a01aa81f
anjiabdallah <anjiabdallah@gmail.com>
2026-04-10 01:26:24 +0300

    movel modal provider into app.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,10 +3,7 @@ import { createRoot } from 'react-dom/client';
 import 'leaflet/dist/leaflet.css';
 import './index.css';
 import App from './App.tsx';
-import { ModalProvider } from './contexts/ModalContext.tsx';
 
 createRoot(document.getElementById('root')!).render(
-  <ModalProvider>
-    <App />
-  </ModalProvider>,
+  <App />,
 );
