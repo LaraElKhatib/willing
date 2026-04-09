cf4ce84f6213c2425675976a50fb6e8de3750220
anjiabdallah <anjiabdallah@gmail.com>
2026-04-10 01:12:33 +0300

    fix the posting page not showing issue

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,5 +3,10 @@ import { createRoot } from 'react-dom/client';
 import 'leaflet/dist/leaflet.css';
 import './index.css';
 import App from './App.tsx';
+import { ModalProvider } from './contexts/ModalContext.tsx';
 
-createRoot(document.getElementById('root')!).render(<App />);
+createRoot(document.getElementById('root')!).render(
+  <ModalProvider>
+    <App />
+  </ModalProvider>,
+);
