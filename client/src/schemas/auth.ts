d22f9a19da0f512212243e32d7bc9193adf4b49a
anjiabdallah <anjiabdallah@gmail.com>
2026-04-11 14:03:00 +0300

    I don't think we need this? (remove z.string for z.email)

--- client/src/schemas/auth.ts
+++ client/src/schemas/auth.ts
@@ -5,7 +5,7 @@ import { passwordSchema } from '../../../server/src/schemas/index.ts';
 export { passwordSchema } from '../../../server/src/schemas/index.ts';
 
 export const loginFormSchema = z.object({
-  email: z.string().min(1, 'Email is required').email('Invalid email'),
+  email: z.email('Invalid email'),
   password: z.string().min(1, 'Password is required'),
 });
 
